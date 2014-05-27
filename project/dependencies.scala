a0f3164eb8387cc1be18993269cf36ad56f825f2
Roberto Tyley <roberto.tyley@gmail.com>
2014-05-27 22:29:33 +0100

    Dependency bump (specs2, guava)

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -4,10 +4,10 @@ object Dependencies {
 
   val jgit = "org.eclipse.jgit" % "org.eclipse.jgit" % "3.3.2.201404171909-r"
 
-  val specs2 = "org.specs2" %% "specs2" % "2.3.11"
+  val specs2 = "org.specs2" %% "specs2" % "2.3.12"
 
   val madgagCompress = "com.madgag" % "util-compress" % "1.33"
 
-  val guava = Seq("com.google.guava" % "guava" % "16.0.1", "com.google.code.findbugs" % "jsr305" % "2.0.1")
+  val guava = Seq("com.google.guava" % "guava" % "17.0", "com.google.code.findbugs" % "jsr305" % "2.0.1")
 
 }

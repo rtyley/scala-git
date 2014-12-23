b76963c86bd7de7b2941fff07c5b6c1b0d30638f
Roberto Tyley <roberto.tyley@guardian.co.uk>
2014-12-23 22:40:20 +0000

    Version bumps

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -3,13 +3,13 @@ import sbt._
 object Dependencies {
 
   val robertoJgit = "com.madgag" % "org.eclipse.jgit" % "3.4.0.1.1-UNOFFICIAL-ROBERTO-RELEASE"
-  val eclipseJgit = "org.eclipse.jgit" % "org.eclipse.jgit" % "3.4.1.201406201815-r"
+  val eclipseJgit = "org.eclipse.jgit" % "org.eclipse.jgit" % "3.5.3.201412180710-r"
   val jgit = eclipseJgit
 
-  val specs2 = "org.specs2" %% "specs2" % "2.3.12"
+  val specs2 = "org.specs2" %% "specs2" % "2.3.13"
 
   val madgagCompress = "com.madgag" % "util-compress" % "1.33"
 
-  val guava = Seq("com.google.guava" % "guava" % "17.0", "com.google.code.findbugs" % "jsr305" % "2.0.1")
+  val guava = Seq("com.google.guava" % "guava" % "18.0", "com.google.code.findbugs" % "jsr305" % "2.0.1")
 
 }

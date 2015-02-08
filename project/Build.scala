8d7a9d5717022e667bf142c61aa4ed7bf1750c73
Roberto Tyley <roberto.tyley@guardian.co.uk>
2015-02-08 16:56:09 +0000

    Update scala version

--- project/Build.scala
+++ project/Build.scala
@@ -8,8 +8,8 @@ import Dependencies._
 object build extends Build {
 
   lazy val baseSettings = ReleasePlugin.releaseSettings ++ sonatypeSettings ++ Seq(
-    scalaVersion := "2.11.4",
-    crossScalaVersions := Seq("2.10.4", "2.11.4"),
+    scalaVersion := "2.11.5",
+    crossScalaVersions := Seq("2.10.4", "2.11.5"),
     organization := "com.madgag.scala-git",
     scmInfo := Some(ScmInfo(
       url("https://github.com/rtyley/scala-git"),
